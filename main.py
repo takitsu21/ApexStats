cbaa7868a0996f2c86da548170df2ae8f28363a2
takitsu21 <dylannbatisse@gmail.com>
2019-03-06 17:15:13 +0100

    embed

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_message(message):
         elif len(args) == 2:
             msg = get_data(pseudo)
         elif message.content == '!apex':
-            embed = (discord.Embed(title="Command: !apex", description="!apex [pseudo]\n!apex [pseudo] [platform] (XBOX,PSN)",colour=0xc8db))
+            embed = (discord.Embed(title="Command: !apex", description="!apex [pseudo]\n!apex [pseudo] [platform] (XBOX,PSN)",colour=0x13c4d4))
         else:
             msg = '{0.author.mention} Un argument manquant ou eronné ```yaml\n!apex [pseudo] [platform](set default to PC if no platform mentionned)```'.format(message)
         if msg:
@@ -50,14 +50,12 @@ async def on_message(message):
         else:
             await client.send_message(message.channel, embed=embed)
         except:
-            embed = (discord.Embed(title="Command: !apex", description="!apex [pseudo]\n!apex [pseudo] [platform](XBOX,PSN)", colour=0xc8db))
+            embed = (discord.Embed(title="Command: !apex", description="!apex [pseudo]\n!apex [pseudo] [platform](XBOX,PSN)", colour=0x13c4d4))
             await client.send_message(message.channel, embed=embed)
 
     if message.content.startswith('!help'):
         embed = (discord.Embed(title='Commands: ', description=' \n'.join(COMMANDS), colour=0xc8db))
         await client.send_message(message.channel, embed=embed)
-
-    if message.content == '!apex':
 
 
 @client.event
