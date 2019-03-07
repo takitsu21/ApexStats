4c55313c5dcda19b328a8db4e7bbee71c9d29b4d
takitsu21 <dylannbatisse@gmail.com>
2019-03-07 18:00:17 +0100

    reddit

--- main.py
+++ main.py
@@ -55,9 +55,15 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
     if message.content.startswith('!reddit'):
-        url_post = ['(' + reddit.hot_post() + ')']
-        embed = (discord.Embed(title='Reddit hot random: ', description='[Reddit hot]' + url_post[0], colour=colour))
-        await client.send_message(message.channel, embed=embed)
+        args = message.content.split(' ')
+        try:
+            reddit_parameter = args[1]
+            if reddit_parameter == 'hot':
+                reddit_hot = str(reddit.hot_post())
+                await client.send_message(message.channel, reddit_hot)
+        except:
+            embed = (discord.Embed(title='Command: !reddit', description='!reddit hot', colour=colour))
+            await client.send_message(message.channel, embed=embed)
 
 @client.event
 async def on_ready():
