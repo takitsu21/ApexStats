187f1cef4aa8c02243ae7bc5254d007611b19449
takitsu21 <dylannbatisse@gmail.com>
2019-03-07 18:49:30 +0100

    z

--- update_github.py
+++ update_github.py
@@ -2,7 +2,5 @@ import os
 
 path = os.path.dirname(os.path.abspath(__file__))
 # os.system('PowerShell')
-os.system('cd {} && git add . && git commit -m "{}" && git push origin master'.format(path,input('commit : ')))
-os.system('takitsu21')
-os.system('takipro21')
+os.system('cd {} && git add . && git commit -m "{}" && git push origin master && takitsu21 && takipro21'.format(path,input('commit : ')))
 # print(os.system('PowerShell ls'))
