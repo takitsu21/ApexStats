07295d11b64a2dd7f508e08644a4dd719f46c06f
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2019-03-18 17:06:07 +0200

    Create APServer.py
    vodka + russian = perfection 👌

---
+++ cogs/APServer.py
@@ -0,0 +1,28 @@
+import discord,asyncio
+from discord.ext import commands
+
+class APServer:
+  def __init__(self,bot):
+    self.bot = bot
+    
+  @commands.command(pass_context=True)
+  async def apserver(self,ctx):
+     try:
+      #
+      Aps = scrap_data.ApexStatus()
+      embed = discord.Embed(title='Apex Servers status', description='{}'.format(Aps.status()), colour=colour)
+      embed.set_thumbnail(url=client.user.avatar_url)
+      embed.set_footer(text="using apexlegendsstatus.com | Bot created by Taki#0853 (WIP)", icon_url=client.user.avatar_url)
+      await ctx.send(embed = embed)
+     except:
+      embed = discord.Embed(title='Apex Servers Status', description='[Apex Server Status](https://apexlegendsstatus.com/datacenters)', colour=colour)
+      embed.set_thumbnail(url=client.user.avatar_url)
+      embed.set_footer(text="Bot created by Taki#0853 (WIP)", icon_url=client.user.avatar_url)
+      await ctx.send(embed=embed)
+      
+      
+#     
+def setup(bot):
+  bot.add_cog(APServer(bot))
+  print("Added APServer cog!")
+  
