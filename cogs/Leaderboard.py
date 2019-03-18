03218987847006ff2b76c9ad0f5bf588e21e8d94
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2019-03-18 16:08:15 +0200

    Leaderboard command
    with a 🍰

---
+++ cogs/Leaderboard.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Leaderboard:
+  def __init__(self,bot):
+    self.bot = bot
+    
+  @commands.command(pass_context=True)
+  async def leaderboard(self,ctx):
+    embed = discord.Embed(title="This command is not yet implemented!",color=0xc8db)
+    await ctx.send(embed=embed)
+    
+def setup(bot):
+  bot.add_cog(Leaderboard(bot))
+  print("Leaderboard was added from cogs")
