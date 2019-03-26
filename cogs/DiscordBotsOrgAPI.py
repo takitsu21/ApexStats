6ad9e8dc3b88552a21e6002dd4224b7d89b4f64b
takitsu21 <dylannbatisse@gmail.com>
2019-03-26 04:26:59 -0700

    post_shard_count

--- cogs/DiscordBotsOrgAPI.py
+++ cogs/DiscordBotsOrgAPI.py
@@ -24,6 +24,7 @@ class DiscordBotsOrgAPI:
             logger.info('attempting to post server count')
             try:
                 await self.dblpy.post_server_count()
+                await self.dblpy.post_shard_count()
                 logger.info('posted server count ({})'.format(len(self.bot.guilds)))
             except Exception as e:
                 logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
