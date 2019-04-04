b828068f0c2abc3dadb06afa0c796f82f7caaa30
takitsu21 <dylannbatisse@gmail.com>
2019-04-04 12:20:14 +0200

    fixed icon url

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_guild_join(ctx):
     embed.add_field(name="**About Apex Stats**",
                     value="Type `a!help` to get all the commands!")
     embed.set_footer(text="Made by Taki#0853 (WIP)",
-                    icon_url=client.guild.me.avatar_url)
+                    icon_url=client.avatar_url)
     await ctx.owner.send(embed = embed)
 
 # @client.event
