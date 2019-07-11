495b678d0159802b70f1dc8257afc83436e3d9d3
takitsu21 <31523130+takitsu21@users.noreply.github.com>
2019-07-11 19:00:29 +0200

    Update DiscordBotsOrgAPI.py

--- cogs/DiscordBotsOrgAPI.py
+++ cogs/DiscordBotsOrgAPI.py
@@ -1,39 +1,33 @@
 import dbl
-import discord, os
+import discord
 from discord.ext import commands
 
-import aiohttp
 import asyncio
 import logging
 
-dblToken = os.environ['DBL_TOKEN']
+dblToken = os.environ["DBL_TOKEN"]
 
-class DiscordBotsOrgAPI:
+class DiscordBotsOrgAPI(commands.Cog):
     """Handles interactions with the discordbots.org API"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = dblToken  #  set this to your DBL token
+        self.token =  # set this to your DBL token
         self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
+        self.updating = self.bot.loop.create_task(self.update_stats())
 
     async def update_stats(self):
-        """This function runs every 60 minutes to automatically update your server count"""
-
-        while True:
-            logger.info('attempting to post server count')
+        """This function runs every 30 minutes to automatically update your server count"""
+        while not self.bot.is_closed():
+            logger.info('Attempting to post server count')
             try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+                await self.dblpy.post_guild_count()
+                logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
             except Exception as e:
                 logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(3600)
-
+            await asyncio.sleep(1800)
 
 def setup(bot):
     global logger
     logger = logging.getLogger('bot')
-    try:
-        bot.add_cog(DiscordBotsOrgAPI(bot))
-    except Exception as e:
-        print(f'{type(e).__name__} : {e}')
+    bot.add_cog(DiscordBotsOrgAPI(bot))
